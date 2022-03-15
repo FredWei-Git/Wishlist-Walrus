4d7a97af61c86cf1e6b1d3bb7cc4e6de97b57165
FredWei-Hub <fredwei2001@gmail.com>
2022-03-14 23:11:41 -0400

    heroku changes

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,16 @@ const usersRouter = require('./routes/users');
 app.use('/items', itemsRouter);
 app.use('/users', usersRouter);
 
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "build")));
+// Step 2:
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
 // starts server/listening to a port
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
