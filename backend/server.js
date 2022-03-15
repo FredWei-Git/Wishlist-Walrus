2df9152e4ed79641394550ddc0c37fb7261d0ed2
FredWei-Hub <fredwei2001@gmail.com>
2022-03-15 00:38:00 -0400

    heroku

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,10 @@ app.use('/items', itemsRouter);
 app.use('/users', usersRouter);
 
 // heroku 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "build")));
 // Handle React routing, return all requests to React app
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // starts server/listening to a port
